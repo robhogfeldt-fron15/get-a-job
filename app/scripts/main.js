9af036fa31571516d3d1a4bc368bf677a5148044
robhogfeldt-fron15 <robhogfeldt@hotmail.com>
2016-01-25 04:18:03 +0100

    linting

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,8 @@
 
-'use strict';
-// import $ from 'jquery';
+"use strict";
+/*eslint no-use-before-define: [2, "nofunc"]*/
+/*global Firebase, $, Handlebars, window, document, console*/
+
 
 var ROOT_URL = "http://robs-cors-server.herokuapp.com/http://api.arbetsformedlingen.se/af/v0/platsannonser/soklista/";
 var AREA_URL = "http://robs-cors-server.herokuapp.com/http://api.arbetsformedlingen.se/af/v0/platsannonser/soklista/lan";
@@ -38,40 +40,41 @@ $(document).ready(function() {
 
 
 
-  $('#textInputBtn').on('click',function(){
-  window.$('#myTab li:eq(2) a').tab('show');
-    var searchStr = $('#textInput').val();
+  $("#textInputBtn").on("click", function(){
+  window.$("#myTab li:eq(2) a").tab("show");
+    var searchStr = $("#textInput").val();
    callTestApi(TEXT_URL + searchStr, "srcStr");
   });
-$('#byArea, #byJob').addClass('hidden');
+$("#byArea, #byJob").addClass("hidden");
 
-$('.collapse').on('hidden.bs.collapse', function () {
-  var target = '#'+$(this).attr('data-parent');
-  $(target).removeClass('collapse-open');
+$(".collapse").on("hidden.bs.collapse", function () {
+  var target = "#" + $(this).attr("data-parent");
+  $(target).removeClass("collapse-open");
 });
 
-$('#areaSearch').on('click',function(){
-  $('#byArea').removeClass('hidden');
-  $('#byJob').addClass('hidden');
-  $('#yrkesannons').find('tbody').empty();
-  callTestApi(AREA_URL,this.id)
+$("#areaSearch").on("click", function(){
+  $("#byArea").removeClass("hidden");
+  $("#byJob").addClass("hidden");
+  $("#yrkesannons").find("tbody").empty();
+  $(".kommunSpin").addClass("show");
+  callTestApi(AREA_URL, this.id);
 });
 
-$('#jobSearch').on('click',function(){
-  $('#byJob').removeClass('hidden');
-  $('#byArea').addClass('hidden');
-  callTestApi(PROF_URL,this.id)
+$("#jobSearch").on("click", function(){
+  $("#byJob").removeClass("hidden");
+  $("#byArea").addClass("hidden");
+  callTestApi(PROF_URL, this.id);
   });
 
 
 
 // SEARCH_BY_AREA -START-
-  $('#lanTempl').on('click', 'td', function(e){
-    $('#kommuner, #yrkesannons').find('tbody').empty();
-    window.$('#myTab li:eq(1) a').tab('show');
-      var id = $(this).attr('id');
-      var name =   $(this).closest('table').attr('id');
-         callTestApi(KOMMUN_URL + id, name)
+  $("#lanTempl").on("click", "td", function(){
+    $("#kommuner, #yrkesannons").find("tbody").empty();
+    window.$("#myTab li:eq(1) a").tab("show");
+      var id = $(this).attr("id");
+      var name = $(this).closest("table").attr("id");
+         callTestApi(KOMMUN_URL + id, name);
 
 
 
@@ -79,52 +82,52 @@ $('#jobSearch').on('click',function(){
 
 
 
-  $('#kommunTempl').on('click', 'td', function(){
-    $('#yrkesannons').find('tbody').empty();
-    window.$('#myTab li:eq(2) a').tab('show');
-        var id = $(this).attr('id');
-        var name =   $(this).closest('table').attr('id');
-         callTestApi(YRKES_IN_AREA + id + '&yrkesomradeid=3', name)
-         $('#item3').addClass('active');
+  $("#kommunTempl").on("click", "td", function(){
+    $("#yrkesannons").find("tbody").empty();
+    window.$("#myTab li:eq(2) a").tab("show");
+        var id = $(this).attr("id");
+        var name = $(this).closest("table").attr("id");
+         callTestApi(YRKES_IN_AREA + id + "&yrkesomradeid=3", name);
+         $("#item3").addClass("active");
   });
 
 
-  $('#areaAdsTempl').on('click', 'td', function(){
-        var id = $(this).attr('id');
-        var name =   $(this).closest('table').attr('id');
-         callTestApi(GET_ADD + id , name)
+  $("#areaAdsTempl").on("click", "td", function(){
+        var id = $(this).attr("id");
+        var name = $(this).closest("table").attr("id");
+         callTestApi(GET_ADD + id, name);
   });
 // SEARCH_BY_AREA -END-
 
 
 
 // var onImageError = function(element) {
-//   console.log('failed to load', element.src)
+//   console.log("failed to load", element.src)
 //   element.remove();
 // }
 
 
 
 // SEARCH_BY_PROFESSION -START-
-  $('#profTempl').on('click', 'td', function(){
-    window.$('#myTabJob li:eq(1) a').tab('show');
-        var id = $(this).attr('id');
-          var name =   $(this).closest('table').attr('id');
-          callTestApi(ROOT_URL + 'yrken?yrkesgruppid='+ id, name)
+  $("#profTempl").on("click", "td", function(){
+    window.$("#myTabJob li:eq(1) a").tab("show");
+        var id = $(this).attr("id");
+          var name = $(this).closest("table").attr("id");
+          callTestApi(ROOT_URL + "yrken?yrkesgruppid=" + id, name);
   });
 
-  $('#yrkesgrupperTempl').on('click', 'td', function(){
-    window.$('#myTabJob li:eq(2) a').tab('show');
-        var id = $(this).attr('id');
-        var name =   $(this).closest('table').attr('id');
-        callTestApi(BYPROF_URL + id, name)
-      $('#item3Job').addClass('active');
+  $("#yrkesgrupperTempl").on("click", "td", function(){
+    window.$("#myTabJob li:eq(2) a").tab("show");
+        var id = $(this).attr("id");
+        var name = $(this).closest("table").attr("id");
+        callTestApi(BYPROF_URL + id, name);
+      $("#item3Job").addClass("active");
   });
-  $('#yrkesAdsTempl').on('click', 'td', function(){
+  $("#yrkesAdsTempl").on("click", "td", function(){
 
-        var id = $(this).attr('id');
-        var name =   $(this).closest('table').attr('id');
-         callTestApi(GET_ADD + id , name)
+        var id = $(this).attr("id");
+        var name = $(this).closest("table").attr("id");
+         callTestApi(GET_ADD + id, name);
   });
   // SEARCH_BY_PROFESSION -END-
 
@@ -133,9 +136,9 @@ $('#jobSearch').on('click',function(){
 
 
 
-  $('#feedTempl').on('click','a', function(){
+  $("#feedTempl").on("click", "a", function(){
 
-      callTestApi(GET_ADD + this.id, "fromLatest")
+      callTestApi(GET_ADD + this.id, "fromLatest");
   });
 
 
@@ -145,76 +148,26 @@ $('#jobSearch').on('click',function(){
 
 
 
-      function callTestApi(url, name) {
-        $('#kommunSpin').addClass('show');
-        $.ajax({
-             url:  url,
-             type: 'GET',
-             success: function(res) {
-                 $('#kommunSpin').removeClass('show');
-                createTable(res,name,url);
-             },
-              error: function(err) { alert(err); },
-          });
-      }
 
 
 
 
-//Kan göras snyggare?
-function createTable(data,name, url) {
-  console.log(name);
-switch (name) {
-  //AREA_CASES
-  case "areaSearch":
-  renderLan(data.soklista.sokdata)
-    break;
-  case "lan":
-  renderKommun(data.soklista.sokdata)
-    break;
-  case "kommuner":
-    renderAreaAds(data.matchningslista.matchningdata)
-    break;
-//PROFESSION_CASES
-  case "jobSearch":
-  renderProf(data.soklista.sokdata)
-    break;
-  case "yrkesgrupper":
-  renderYrkesgrupper(data.soklista.sokdata)
-      break;
-  case "yrken":
-  renderYrkesAds(data.matchningslista.matchningdata)
-      break;
-  case "yrkesannons":
-  renderAdd(data.platsannons.annons)
-      break;
-  case "fromLatest":
-  renderAddfromLatest(data.platsannons.annons, url, myFunction)
-      break;
-  case "srcStr":
-  renderAddsByStr(data.matchningslista.matchningdata)
-      break;
-    default:
-      console.log('oh no');
-      break;
-  }
-}
+
 
 
 function renderAddsByStr(data) {
   console.log(data);
-  var stamp = Date.now()
   var timestamp = new Date();
   var time = timestamp.toTimeString();
   var day = timestamp.toDateString();
-  var templ = $('#areaAdsTemplate').html(),
+  var templ = $("#areaAdsTemplate").html(),
    compiled = Handlebars.compile(templ),
    rendered = compiled({areaAdd: data});
-   $('#areaAdsTempl').html(rendered);
+   $("#areaAdsTempl").html(rendered);
 
    var latestRef = myFirebaseRef.child("searchStr");
    latestRef.push({
-       string: $('#textInput').val(),
+       string: $("#textInput").val(),
        hits: data.length,
        day: day,
        time: time,
@@ -224,66 +177,66 @@ function renderAddsByStr(data) {
 
 
 function renderLan(data) {
-  var templ = $('#lanTemplate').html(),
+  var templ = $("#lanTemplate").html(),
    compiled = Handlebars.compile(templ),
    rendered = compiled({lan: data});
-   $('#lanTempl').html(rendered);
+   $("#lanTempl").html(rendered);
 }
 
 function renderKommun(data) {
-  var templ = $('#kommunTemplate').html(),
+  var templ = $("#kommunTemplate").html(),
    compiled = Handlebars.compile(templ),
    rendered = compiled({kommun: data});
-   $('#kommunTempl').html(rendered);
+   $("#kommunTempl").html(rendered);
 }
 
 function renderAreaAds(data) {
 console.log(data);
-  var templ = $('#areaAdsTemplate').html(),
+  var templ = $("#areaAdsTemplate").html(),
    compiled = Handlebars.compile(templ),
    rendered = compiled({areaAdd: data});
-   $('#areaAdsTempl').html(rendered);
+   $("#areaAdsTempl").html(rendered);
 }
 
 function renderProf(data) {
 
-  var templ = $('#profTemplate').html(),
+  var templ = $("#profTemplate").html(),
    compiled = Handlebars.compile(templ),
    rendered = compiled({prof: data});
-   $('#profTempl').html(rendered);
+   $("#profTempl").html(rendered);
 }
 
 function renderYrkesgrupper(data) {
 
-  var templ = $('#yrkesgrupperTemplate').html(),
+  var templ = $("#yrkesgrupperTemplate").html(),
    compiled = Handlebars.compile(templ),
    rendered = compiled({yrkesgrupp: data});
-   $('#yrkesgrupperTempl').html(rendered);
+   $("#yrkesgrupperTempl").html(rendered);
 }
 
 
 
 function renderYrkesAds(data) {
-  console.log(data);;
-  var templ = $('#yrkesAdsTemplate').html(),
+  console.log(data);
+  var templ = $("#yrkesAdsTemplate").html(),
    compiled = Handlebars.compile(templ),
    rendered = compiled({yrkesAdd: data});
-   $('#yrkesAdsTempl').html(rendered);
+   $("#yrkesAdsTempl").html(rendered);
 }
 
 
 function renderAdd(data) {
 console.log(data);
-  data.logo = GET_LOGO + data.annonsid + '/logotyp';
-var stamp = Date.now()
+  data.logo = GET_LOGO + data.annonsid + "/logotyp";
+
 var timestamp = new Date();
 var time = timestamp.toTimeString();
 var day = timestamp.toDateString();
 
-  var templ = $('#adTemplate').html();
+  var templ = $("#adTemplate").html();
   var compiled = Handlebars.compile(templ);
   var theCompiledHtml = compiled(data);
-  $('#adTempl').html(theCompiledHtml);
+  $("#adTempl").html(theCompiledHtml);
 
 var latestRef = myFirebaseRef.child("latest");
 latestRef.push({
@@ -298,58 +251,115 @@ latestRef.push({
 }
 
 
-function renderAddfromLatest(data, url) {
-
-
-  data.logo = GET_LOGO + data.annonsid + '/logotyp';
-  var templ = $('#adTemplate').html();
+function renderAddfromLatest(data) {
+  data.logo = GET_LOGO + data.annonsid + "/logotyp";
+  var templ = $("#adTemplate").html();
   var compiled = Handlebars.compile(templ);
   var theCompiledHtml = compiled(data);
-  $('#adTempl').html(theCompiledHtml);
+  $("#adTempl").html(theCompiledHtml);
 
-
-
-};
+}
 
  window.myFunction = function(el) {
    el.remove();
-  console.log('failed to load', el.src)
-
-}
+  console.log("failed to load", el.src);
+};
 
 
 function renderFeed(data) {
 console.log(data);
-data.logo = GET_LOGO + data.id + '/logotyp';
-  var templ = $('#feedTemplate').html();
+data.logo = GET_LOGO + data.id + "/logotyp";
+  var templ = $("#feedTemplate").html();
   var compiled = Handlebars.compile(templ);
   var theCompiledHtml = compiled(data);
-  $('#feedTempl').prepend(theCompiledHtml);
+  $("#feedTempl").prepend(theCompiledHtml);
   $("#feedTempl").fadeIn(1000);
-};
+
+//DISPLAY LATEST FIVE
+var count = $("#feedTempl div.childWrap").length;
+if (count > 5) {$("#feedTempl div:last-child").remove();
+  var $newRow = $("#feedTempl div:first-child");
+  $newRow.effect("highlight", {}, 3000);
+  }
+}
 
 function renderWordFeed(data) {
 console.log(data);
-data.logo = GET_LOGO + data.id + '/logotyp';
-  var templ = $('#wordFeedTemplate').html();
+data.logo = GET_LOGO + data.id + "/logotyp";
+  var templ = $("#wordFeedTemplate").html();
   var compiled = Handlebars.compile(templ);
   var theCompiledHtml = compiled(data);
-  $('#wordFeedTempl').prepend(theCompiledHtml);
+  $("#wordFeedTempl").prepend(theCompiledHtml);
   $("#wordFeedTempl").fadeIn(1000);
+}
+
+
+//Kan göras snyggare?
+var createTable = function(data, name, url) {
+  console.log(name);
+switch (name) {
+  //AREA_CASES
+  case "areaSearch":
+  renderLan(data.soklista.sokdata);
+    break;
+  case "lan":
+  renderKommun(data.soklista.sokdata);
+    break;
+  case "kommuner":
+    renderAreaAds(data.matchningslista.matchningdata);
+    break;
+//PROFESSION_CASES
+  case "jobSearch":
+  renderProf(data.soklista.sokdata);
+    break;
+  case "yrkesgrupper":
+  renderYrkesgrupper(data.soklista.sokdata);
+      break;
+  case "yrken":
+  renderYrkesAds(data.matchningslista.matchningdata);
+      break;
+  case "yrkesannons":
+  renderAdd(data.platsannons.annons);
+      break;
+  case "fromLatest":
+  renderAddfromLatest(data.platsannons.annons, url, this.myFunction);
+      break;
+  case "srcStr":
+  renderAddsByStr(data.matchningslista.matchningdata);
+      break;
+    default:
+      console.log("oh no");
+      break;
+  }
 };
 
+function callTestApi(url, name) {
+  $(".mdl-spinner").addClass("show");
+  $.ajax({
+       url: url,
+       type: "GET",
+       success: function(res) {
+
+    createTable(res, name, url );
+          $(".mdl-spinner").removeClass("show");
+       },
+        error: function(err) {
+          console.log(err);
+        }
+    });
+}
 //Helpers
 
-Handlebars.registerHelper('trimString', function(passedString) {
-    var theString = passedString.substring(0,500);
-    return new Handlebars.SafeString(theString + "...")
+Handlebars.registerHelper("trimString", function(passedString) {
+    var theString = passedString.substring(0, 500);
+    return new Handlebars.SafeString(theString + "...");
 });
 
-Handlebars.registerHelper('trimDate', function(passedString) {
-    var theString = passedString.substring(0,9);
-    return new Handlebars.SafeString(theString)
+Handlebars.registerHelper("trimDate", function(passedString) {
+    var theString = passedString.substring(0, 9);
+    return new Handlebars.SafeString(theString);
 });
 
-Handlebars.registerHelper('myFunction', function(el) {
+Handlebars.registerHelper("myFunction", function(el) {
   el.remove();
 });
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,25 +1,31 @@
 // generated on 2016-01-23 using generator-gulp-webapp 1.0.4
-import gulp from 'gulp';
-import gulpLoadPlugins from 'gulp-load-plugins';
-import browserSync from 'browser-sync';
-import del from 'del';
-import {stream as wiredep} from 'wiredep';
+"use strict";
+import gulp from "gulp";
+import gulpLoadPlugins from "gulp-load-plugins";
+import browserSync from "browser-sync";
+import del from "del";
+import {stream as wiredep} from "wiredep";
 
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
 
-gulp.task('styles', () => {
-  return gulp.src('app/styles/*.scss')
+var onError = function(err) {
+    console.log(err);
+};
+
+
+gulp.task("styles", () => {
+  return gulp.src("app/styles/*.scss")
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
     .pipe($.sass.sync({
-      outputStyle: 'expanded',
+      outputStyle: "expanded",
       precision: 10,
-      includePaths: ['.']
-    }).on('error', $.sass.logError))
-    .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
+      includePaths: ["."]
+    }).on("error", $.sass.logError))
+    .pipe($.autoprefixer({browsers: ["> 1%", "last 2 versions", "Firefox ESR"]}))
     .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('.tmp/styles'))
+    .pipe(gulp.dest(".tmp/styles"))
     .pipe(reload({stream: true}));
 });
 
@@ -27,8 +33,8 @@ function lint(files, options) {
   return () => {
     return gulp.src(files)
       .pipe(reload({stream: true, once: true}))
-      // .pipe($.eslint(options))
-      // .pipe($.eslint.format())
+      .pipe($.eslint(options))
+      .pipe($.eslint.format())
       .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
   };
 }
@@ -38,135 +44,136 @@ const testLintOptions = {
   }
 };
 
-gulp.task('lint', lint('app/scripts/**/*.js'));
-gulp.task('lint:test', lint('test/spec/**/*.js', testLintOptions));
-
-// gulp.task('html', ['styles'], () => {
-//   return gulp.src('app/*.html')
-//     .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
-//     .pipe($.if('*.js', $.uglify()))
-//     .pipe($.if('*.css', $.cssnano()))
-//     .pipe($.if('*.html', $.htmlmin()))
-//     .pipe(gulp.dest('dist'));
-// });
-
-gulp.task('html', ['styles'], () => {
-  return gulp.src('app/*.html')
-    .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
-    .pipe($.if('*.js', $.uglify()))
-    .pipe($.if('*.css', $.cssnano()))
-    .pipe($.if('*.html', $.htmlmin()))
-    .pipe(gulp.dest('dist'));
+gulp.task("jshint", function() {
+    return gulp.src("app/scripts/**/*.js")
+    .pipe($.plumber({
+        errorHandler: onError
+    }))
+    .pipe($.jshint())
+    .pipe($.jshint.reporter("default"))
+    .pipe($.notify({ message: "JS Hinting task complete" }));
 });
 
-gulp.task('images', () => {
-  return gulp.src('app/images/**/*')
+gulp.task("lint", lint("app/scripts/**/*.js"));
+gulp.task("lint:test", lint("test/spec/**/*.js", testLintOptions));
+
+gulp.task("html", ["styles"], () => {
+  return gulp.src("app/*.html")
+    .pipe($.useref({searchPath: [".tmp", "app", "."]}))
+    .pipe($.if("*.js", $.uglify()))
+    .pipe($.if("*.css", $.cssnano()))
+    .pipe($.if("*.html", $.htmlmin()))
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("images", () => {
+  return gulp.src("app/images/**/*")
     .pipe($.if($.if.isFile, $.cache($.imagemin({
       progressive: true,
       interlaced: true,
-      // don't remove IDs from SVGs, they are often used
+      // don"t remove IDs from SVGs, they are often used
       // as hooks for embedding and styling
       svgoPlugins: [{cleanupIDs: false}]
     }))
-    .on('error', function (err) {
+    .on("error", function (err) {
       console.log(err);
       this.end();
     })))
-    .pipe(gulp.dest('dist/images'));
+    .pipe(gulp.dest("dist/images"));
 });
 
-gulp.task('fonts', () => {
-  return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
-    .concat('app/fonts/**/*'))
-    .pipe(gulp.dest('.tmp/fonts'))
-    .pipe(gulp.dest('dist/fonts'));
+gulp.task("fonts", () => {
+  return gulp.src(require("main-bower-files")("**/*.{eot,svg,ttf,woff,woff2}", function (err) {console.log(err); })
+    .concat("app/fonts/**/*"))
+    .pipe(gulp.dest(".tmp/fonts"))
+    .pipe(gulp.dest("dist/fonts"));
 });
 
 
 
-gulp.task('extras', () => {
+gulp.task("extras", () => {
   return gulp.src([
-    'app/*.*',
-    '!app/*.html'
+    "app/*.*",
+    "!app/*.html"
   ], {
     dot: true
-  }).pipe(gulp.dest('dist'));
+  }).pipe(gulp.dest("dist"));
 });
 
-gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+gulp.task("clean", del.bind(null, [".tmp", "dist"]));
 
-gulp.task('serve', ['styles', 'fonts'], () => {
+gulp.task("serve", ["styles", "fonts"], () => {
   browserSync({
     notify: false,
     port: 9000,
     server: {
-      baseDir: ['.tmp', 'app'],
+      baseDir: [".tmp", "app"],
       routes: {
-        '/bower_components': 'bower_components'
+        "/bower_components": "bower_components"
       }
     }
   });
 
   gulp.watch([
-    'app/*.html',
-    'app/scripts/**/*.js',
-    'app/images/**/*',
-    '.tmp/fonts/**/*'
-  ]).on('change', reload);
+    "app/*.html",
+    "app/scripts/**/*.js",
+    "app/images/**/*",
+    ".tmp/fonts/**/*"
+  ]).on("change", reload);
 
-  gulp.watch('app/styles/**/*.scss', ['styles']);
-  gulp.watch('app/fonts/**/*', ['fonts']);
-  gulp.watch('bower.json', ['wiredep', 'fonts']);
+  gulp.watch("app/styles/**/*.scss", ["styles"]);
+  gulp.watch("app/fonts/**/*", ["fonts"]);
+  gulp.watch("bower.json", ["wiredep", "fonts"]);
 });
 
-gulp.task('serve:dist', () => {
+gulp.task("serve:dist", () => {
   browserSync({
     notify: false,
     port: 9000,
     server: {
-      baseDir: ['dist']
+      baseDir: ["dist"]
     }
   });
 });
 
-gulp.task('serve:test', () => {
+gulp.task("serve:test", () => {
   browserSync({
     notify: false,
     port: 9000,
     ui: false,
     server: {
-      baseDir: 'test',
+      baseDir: "test",
       routes: {
-        '/scripts': 'app/scripts',
-        '/bower_components': 'bower_components'
+        "/scripts": "app/scripts",
+        "/bower_components": "bower_components"
       }
     }
   });
 
-  gulp.watch('test/spec/**/*.js').on('change', reload);
-  gulp.watch('test/spec/**/*.js', ['lint:test']);
+  gulp.watch("test/spec/**/*.js").on("change", reload);
+  gulp.watch("test/spec/**/*.js", ["lint:test"]);
 });
 
 // inject bower components
-gulp.task('wiredep', () => {
-  gulp.src('app/styles/*.scss')
+gulp.task("wiredep", () => {
+  gulp.src("app/styles/*.scss")
     .pipe(wiredep({
       ignorePath: /^(\.\.\/)+/
     }))
-    .pipe(gulp.dest('app/styles'));
+    .pipe(gulp.dest("app/styles"));
 
-  gulp.src('app/*.html')
+  gulp.src("app/*.html")
     .pipe(wiredep({
-      exclude: ['bootstrap-sass'],
+      exclude: ["bootstrap-sass"],
       ignorePath: /^(\.\.\/)*\.\./
     }))
-    .pipe(gulp.dest('app'));
+    .pipe(gulp.dest("app"));
 });
 
-gulp.task('build', ['html', 'lint','images', 'fonts'], () => {
-  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+gulp.task("build", ["html", "lint", "images", "fonts"], () => {
+  return gulp.src("dist/**/*").pipe($.size({title: "build", gzip: true}));
 });
 
-gulp.task('default', ['clean'], () => {
-  gulp.start('build');
+gulp.task("default", ["clean"], () => {
+  gulp.start("build");
 });
